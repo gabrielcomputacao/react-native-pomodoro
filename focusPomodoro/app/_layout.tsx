0ffc09c511d203db96b4b7fc5420189f94720cd6
Gabriel Henrique <gabrielcomputacao@outlook.com>
2026-04-19 20:03:37 -0300

    feat: including menus in the drawer and SafeAreaView

--- focusPomodoro/app/_layout.tsx
+++ focusPomodoro/app/_layout.tsx
@@ -1,5 +1,52 @@
 import { Drawer } from "expo-router/drawer";
 
 export default function Layout() {
-  return <Drawer></Drawer>;
+  return (
+    <Drawer
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#021123",
+        },
+        headerTintColor: "#fff",
+        drawerStyle: {
+          backgroundColor: "#021123",
+        },
+        drawerLabelStyle: {
+          color: "#fff",
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          drawerItemStyle:{
+            display: 'none'
+          }
+        }}
+      />
+      <Drawer.Screen
+        name="add-task/index"
+        options={{
+          drawerItemStyle: {
+            display: 'none'
+          }
+        }}
+      />
+      <Drawer.Screen
+        name="pomodoro"
+        options={{
+         title: '',
+         drawerLabel: 'Timer'
+        }}
+      />
+      <Drawer.Screen
+        name="task/index"
+        options={{
+         title: '',
+         drawerLabel: 'Lista de tarefas'
+        }}
+      />
+    </Drawer>
+  );
 }
